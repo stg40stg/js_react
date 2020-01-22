7de6e87f5ab5fd83efd08457eb1ed81957b8eb0c
stg40 <stg40stg@gmail.com>
2020-01-22 18:04:21 +0300

    Start new lesson on React

---
+++ App/static_src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1 className="element">Кажется, мы подключили React</h1>;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root'),
+);
+
+
