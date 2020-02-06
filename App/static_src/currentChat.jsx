0be552b1a5b94ab77442210f30aad839b9b0f4c8
stg40 <stg40stg@gmail.com>
2020-02-06 15:00:36 +0300

    homework for lesson 4 done

--- App/static_src/currentChat.jsx
+++ App/static_src/currentChat.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Messages from "./messages.jsx";
 import {FloatingActionButton, TextField} from "material-ui";
 import SendIcon from "material-ui/svg-icons/content/send";
-import Header from "./header.jsx";
 
 export default class CurrentChat extends React.Component {
     static propTypes = {
@@ -19,31 +18,12 @@ export default class CurrentChat extends React.Component {
     }
 
     state = {
-      message: ''
+        message: ''
     };
 
-
-
-    // componentDidUpdate(prevProps, prevState) {
-    //     let messages = [...this.state.messages];
-    //     if (messages.length > 0) {
-    //         if (messages.length > prevState.messages.length) {
-    //             let lastMessage = messages[messages.length - 1];
-    //             if (lastMessage.author === 'user') {
-    //                 setTimeout(() =>
-    //                     this.setState(
-    //                         {
-    //                             messages: [...this.state.messages, {
-    //                                 author: 'bot',
-    //                                 text: answers[Math.floor(Math.random() * Math.floor(answers.length))]
-    //                             }]
-    //                         }
-    //                     ), 1000);
-    //             }
-    //         }
-    //     }
-    //     this.scrollToBottom();
-    // }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.scrollToBottom();
+    }
 
     componentDidMount() {
         this.textInput.current.focus();
@@ -53,26 +33,10 @@ export default class CurrentChat extends React.Component {
         if (this.chatContainer.current)
             this.chatContainer.current.scrollTop = this.chatContainer.current.scrollHeight;
     }
-    // sendMessage = (message, author) => {
-    //     const { messages, chats } = this.state;
-    //     const { chatId } = this.props;
-    //
-    //     const messageId = Object.keys(messages).length + 1;
-    //     this.setState({
-    //         messages: {...messages,
-    //             [messageId]: {text: message, author: author}},
-    //         chats: {...chats,
-    //             [chatId]: { ...chats[chatId],
-    //                 messageList: [...chats[chatId]['messageList'], messageId]
-    //             }
-    //         },
-    //     })
-    // };
 
     render() {
         return (
-            <div>
-                <Header title={this.props.chat.name}/>
+            <div className="form-wrap">
                 <form className="main" onSubmit={(evt) => {
                     this.props.addMessage(this.state.message);
                     this.setState({
@@ -83,15 +47,16 @@ export default class CurrentChat extends React.Component {
 
                     <Messages
                         messages={this.props.chat.messages}
+                        ref={this.chatContainer}
                     />
                     <div className="message-wrap">
                         <TextField type="text" hintText="Введите сообщение" value={this.state.message}
                                    ref={this.textInput} onChange={(evt) => {
-                                       this.setState({message: evt.target.value});
+                            this.setState({message: evt.target.value});
                         }}/>
+                        <FloatingActionButton type="submit" className="sendButton" mini={true}
+                                   value="Отправить"><SendIcon/></FloatingActionButton>
                     </div>
-                    <FloatingActionButton type="submit" className="sendButton" mini={true}
-                                          value="Отправить"><SendIcon/></FloatingActionButton>
                 </form>
             </div>
         );
--- App/static_src/messages.jsx
+++ App/static_src/messages.jsx
@@ -1,34 +1,20 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-
-
 export default class Messages extends React.Component {
     static propTypes = {
         messages: PropTypes.array.isRequired,
     };
 
     render() {
-        // const { chatId, chats, messages } = this.props;
-        //
-        // const messageElements = chats[chatId].messageList.map(messageId => (
-        //     <Message
-        //         key={ messageId }
-        //         text={ messages[messageId].text }
-        //         sender={ messages[messageId].author }
-        //     />
-        // ));
         return (
-            <div>
-                {/*<div>*/}
-                {/*    {messageElements}*/}
-                {/*</div>*/}
-                <div className="main-window" ref={this.chatContainer}>
+            <div className="messages">
+                <div className="main-window">
                     {
                         this.props.messages.map((message, index) => {
                             return (<div key={index} className="message"
                                          style={{
-                                             alignSelf: message.author === 'user' ? 'flex-start' : 'flex-end'
+                                             alignSelf: message.author === 'Me' ? 'flex-start' : 'flex-end'
                                          }}>
                                 <span className="authorName">{message.author}</span><br/>{message.text}<br/>
                             </div>)
